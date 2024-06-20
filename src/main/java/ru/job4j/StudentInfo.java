8ea9ab168db2737027e3fd40a571088127f859e0
ofiel <ofiel@inbox.ru>
2024-06-20 19:16:04 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Matrosova Lyubov");
+        System.out.println("05.02.1989");
+    }
+}
