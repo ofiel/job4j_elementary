570468422b0806f64c77b899235feb2ae82add85
ofiel <ofiel@inbox.ru>
2024-06-22 00:19:43 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Multiple {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("1*1=1\n1*2=2\n1*3=3\n1*4=4\n1*5=5\n1*6=6\n1*7=7\n1*8=8\n1*9=9\n1*10=10");
         System.out.println("Done!!!");
     }
--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Matrosova Lyubov");
         System.out.println("05.02.1989");
     }
