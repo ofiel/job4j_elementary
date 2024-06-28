9715b595280c921fed761a289c970c8b8824c853
ofiel <ofiel@inbox.ru>
2024-06-28 12:46:36 +0300

    9. Площадь треугольника. [#175474 #502704]

--- src/main/java/ru/job4j/calculator/TriangleArea.java
+++ src/main/java/ru/job4j/calculator/TriangleArea.java
@@ -3,7 +3,7 @@ package ru.job4j.calculator;
 public class TriangleArea {
     public static double area(double a, double b, double c) {
         double p = (a + b + c) / 2;
-        double result = Math.sqrt((p - a) * (p - b) * (p - c));
+        double result = Math.sqrt(p * ((p - a) * (p - b) * (p - c)));
         return result;
     }
 
