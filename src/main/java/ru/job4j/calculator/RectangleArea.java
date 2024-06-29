96e1bc4827d666c820f5df06f3b701a0f4055128
ofiel <ofiel@inbox.ru>
2024-06-29 21:29:39 +0300

    4. Тесты для стороны прямоугольника. [#159460]

--- src/main/java/ru/job4j/calculator/RectangleArea.java
+++ src/main/java/ru/job4j/calculator/RectangleArea.java
@@ -12,8 +12,10 @@ public class RectangleArea {
     public static void main(String[] args) {
         double result = RectangleArea.square(4, 1);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result);
-        double result2 = RectangleArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result2);
+        double result1 = RectangleArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+        double result2 = RectangleArea.square(10, 4);
+        System.out.println(" p = 10, k = 6, s = 4, real = " + result2);
 
     }
 }
---
+++ src/test/java/ru/job4j/calculator/RectangleAreaTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class RectangleAreaTest {
+
+    @Test
+    void whenP4K1Square1() {
+        int expected = 1;
+        int p = 4;
+        double k = 1;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+
+    void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+
+    void whenP10K4Square4() {
+        int expected = 4;
+        int p = 10;
+        double k = 4;
+        double output = RectangleArea.square(p, k);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
