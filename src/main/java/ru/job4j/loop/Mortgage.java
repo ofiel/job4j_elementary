c7e7fdf96c2f8d383f35b9f4c2900627e26ffe45
ofiel <ofiel@inbox.ru>
2024-07-11 09:28:33 +0300

    5.7. Ипотека [#156315 #504499]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,8 +4,8 @@ public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount = amount + (amount * percent) / 100;
-            amount = amount - salary;
+            amount += (amount * percent) / 100;
+            amount -= salary;
             year++;
         }
         return year;
