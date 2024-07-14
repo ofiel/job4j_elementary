a6e9918c4417092f13e9d57b0155a0387900bb13
ofiel <ofiel@inbox.ru>
2024-07-14 15:20:10 +0300

    6.4.0. Слово начинается с ... [#41585 #504919]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,12 +4,11 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
         for (int i = 0; i < prefix.length; i++) {
-            if (prefix[i] == word[i]) {
-                result = true;
-            } else {
-            result = false;
-            break;
+            if (prefix[i] != word[i]) {
+                result = false;
+                break;
             }
+
         }
         return result;
     }
