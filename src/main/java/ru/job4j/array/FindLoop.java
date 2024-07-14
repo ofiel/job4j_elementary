08ae82c5e52a2d0c37c35b3e9634a5f49ece8f57
ofiel <ofiel@inbox.ru>
2024-07-14 18:54:05 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -2,9 +2,20 @@ package ru.job4j.array;
 
 public class FindLoop {
     public static int indexOf(int[] data, int element) {
-        int result = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        int result = -1;
         for (int i = 0; i < data.length; i++) {
             if (data[i] == element) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static int indexInRange(int[] data, int element, int start, int finish) {
+        int result = -1;
+        for (int i = 0; i < data.length; i++) {
+            if (i >= start && i <= finish && data[i] == element) {
                 result = i;
                 break;
             }
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FindLoopTest {
 
     @Test
-    void whenArrayHas5Then0() {
+    public void whenArrayHas5Then0() {
         int[] data = new int[] {5, 10, 3};
         int element = 5;
         int result = FindLoop.indexOf(data, element);
@@ -15,24 +15,46 @@ class FindLoopTest {
     }
 
     @Test
-    void whenArrayHasNot10ThenMinus1() {
-        /* Здесь нужно дописать тест, когда в массиве не найдено число 10.
-        Можно искать любое другое число, корректно изменив при этом название теста */
-        int[] data = new int[] {3, 4, 5};
-        int element = 10;
-        int result = FindLoop.indexOf(data, element);
-        int expected = -1;
+    public void whenDiapasonHas8ThenResultEqualFinish() {
+        int[] data = new int[] {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int element = 8;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenArrayHas4Then1() {
-        /* Здесь нужно дописать тест, когда в массиве не найдено число 10.
-        Можно искать любое другое число, корректно изменив при этом название теста */
-        int[] data = new int[] {3, 4, 5};
+    public void whenDiapasonHasSomeEqualElementThen3() {
+        int[] data = new int[] {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
         int element = 4;
-        int result = FindLoop.indexOf(data, element);
-        int expected = 1;
+        int start = 1;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = 3;
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int element = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+        int[] data = new int[] {1, 5, 4, 2, 6, 9, 22};
+        int element = 8;
+        int start = 0;
+        int finish = 5;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = 8;
+        assertThat(result).isNotEqualTo(expected);
     }
 }
